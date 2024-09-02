9f7feb038171d9f4579668743e87255bed5ce07a
Alek V <alekvasek@icloud.com>
2024-09-02 16:07:21 -0500

    Updated Code

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import keyboard
 
 print(mido.get_input_names())
 
-note_dict = {
+
+
+
+def main():
+    note_dict = {
     0: "C",
     1: "C#",
     2: "D",
@@ -20,69 +24,101 @@ note_dict = {
     11: "B"}
 
 
-current_notes = []
-session_notes = []
-session_note_values = []
+    char_dict = {
+        'A': 'A',
+        'B': 'B',
+        'C': 'C',
+        'D': 'D',
+        'E': 'E',
+        'F': 'F',
+        'G': 'G',
+        'Am': 'H',
+        'Bm': 'I',
+        'Cm': 'J',
+        'Dm': 'K',
+        'Em': 'L',
+        'Fm': 'M',
+        'Gm': 'N',
+        'A7': 'O',
+        'B7': 'P',
+        'C7': 'Q',
+        'D7': 'R',
+        'E7': 'S',
+        'F7': 'T',
+        'G7': 'U',
+        'Asus2': 'V',
+        'Bsus2': 'W',
+        'Csus2': 'X',
+        'Dsus2': 'Y',
+        'Esus2': 'Z'
+    }
+
+    current_notes = []
+    session_notes = []
+    session_note_values = []
 
 
+    with mido.open_input("KL Essential 61 mk3 MIDI") as inport:
+            while True:
 
-try:
-    while True:
-        with mido.open_input("KL Essential 61 mk3 MIDI") as inport:
+                for msg in inport:
+                    note = {note_dict[(msg.note)%12]: (msg.note-24)//12}
+                    note_value = msg.note
 
-            for msg in inport:
-                note = {note_dict[(msg.note)%12]: (msg.note-24)//12}
-                note_value = msg.note
+                    if msg.type == "note_on":
+                        current_notes.append(note)
+                        if note not in session_notes:
+                            session_notes.append(note)
+                            session_note_values.append(note_value)
 
-                if msg.type == "note_on":
-                    current_notes.append(note)
-                    if note not in session_notes:
-                        session_notes.append(note)
-                        session_note_values.append(note_value)
+                    if msg.type == "note_off":
 
-                if msg.type == "note_off":
-
-                    current_notes.remove(note)
+                        current_notes.remove(note)
 
 
-                if current_notes == []:
-                    session_notes.sort(key=lambda x: list(x.values())[0])
-                    session_notes.sort(key=lambda x: list(x.keys())[0])
+                    if current_notes == []:
+                        session_notes.sort(key=lambda x: list(x.values())[0])
+                        session_notes.sort(key=lambda x: list(x.keys())[0])
+                        session_note_values.sort()
 
-                    if len(session_notes) == 2:
-                        if session_notes[0] != session_notes[1]:
-                                number = session_note_values[0] - session_note_values[1]
-                                print("Number: ", number)
-                                
-                    elif len(session_notes) == 3:
+                        if len(session_notes) == 2:
+                            if session_notes[0] != session_notes[1]:
+                                    number = session_note_values[1] - session_note_values[0]
+                                    print("Number: ", number)
+ 
 
-                        print("Current notes: ", session_notes)
+                                    keyboard.write(f"{number}")
+                                    
+                        elif len(session_notes) == 3:
 
-                        note_list = []
-                        for note in session_notes:
-                            note_list.append(list(note.keys())[0])
+                            print("Current notes: ", session_notes)
+
+                            note_list = []
+                            for note in session_notes:
+                                note_list.append(list(note.keys())[0])
+                            
+                            chord = str(find_chords_from_notes(note_list)[0])
+
+                            chord.replace("[<Chord: ", "").replace(" >]", "")
+
+                            print("Chord: ", chord) 
+
+                            if chord in list(char_dict.keys()):
+                                char = char_dict[chord]
+
+                                keyboard.write(char)
+                        else:
+                            pass
+
+                            
                         
-                        print("Chords: ", find_chords_from_notes(note_list))
+                        session_notes = []
+                        session_note_values = []
 
-                    elif len(session_notes) == 4:
-                        print("Current notes: ", session_notes)
-
-                        note_list = []
-                        for note in session_notes[:1]:
-                            note_list.append(list(note.keys())[0])
-
-                        over_note = list(session_notes[0].keys())[0]
-
-                        print("Chords: ", find_chords_from_notes(note_list), "/", over_note)
-
-                        
-                    
-                    session_notes = []
-
-                
-                
-
-except KeyboardInterrupt:
-    sys.exit(0)
+if __name__ == "__main__":
+     try:
+         main()
+     except KeyboardInterrupt:
+         sys.exit(0)
 
 
